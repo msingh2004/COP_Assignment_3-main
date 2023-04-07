3076b7605523c1a96710cc408bd8f71d171d26ac
msingh2004 <kabir.ms.2004@gmail.com>
2023-04-07 22:15:31 +0530

    Few changes

--- app.py
+++ app.py
@@ -176,5 +176,12 @@ def profile():
 def following():
     return render_template('following.html')
 
+@app.route('/logout')
+def logout():
+    session.pop('username', None)
+    return redirect(url_for('home'))
+
+
+
 if __name__ == "__main__":
     app.run(debug=True)
